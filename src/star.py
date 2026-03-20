7b8a8de84fcd2726b72745ec174a58df54c5ea11
corpaul123 <111656005+corpaul123@users.noreply.github.com>
2026-03-20 21:03:17 +0000

    Continued setup for common.py, center of mass.

--- src/star.py
+++ src/star.py
@@ -2,7 +2,7 @@ def star():
     starDict = {}
     mFinal = pow(10, 30)
     starName = input("Input name of star: ")
-    mass = float(input("Input mass of star (in 10 ^ 30 kg): "))
+    mass = float(input("Input mass of star (in 10 ^ 30 kg, Sun's mass = 1.9): "))
     actualMass = mass * mFinal
     print(f"Star name is {starName}")
     starDict = {starName : {
