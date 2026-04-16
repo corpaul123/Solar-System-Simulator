3599611707c8646840f8c1be728e7524cc860bf7
corpaul123 <111656005+corpaul123@users.noreply.github.com>
2026-04-16 20:08:20 +0000

    Improved planet entry, allowing for multiple planets and verifying that planets do not overlap.

--- src/planet.py
+++ src/planet.py
@@ -2,9 +2,19 @@ from addPlanet import addPlanet
 
 def planets(starDict):
     planetsDict = {}
+    radiSet = set()
 
     print("ready for planets")
-    addPlanet(planetsDict, starDict)
+    done = False
+    while done == False:
+        planetNum = int(input("Input number of planets: "))
+        if planetNum > 0 and planetNum < 20:
+            done = True
+        else:
+            print("Please enter valid planet amount.")
+    
+    for i in range(planetNum):
+        addPlanet(planetsDict, starDict, radiSet)
     print("Here is planet data")
     print(planetsDict)
 
